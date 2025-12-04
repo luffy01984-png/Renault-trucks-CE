0a48b2e847b38c1a8c9cdbfce8fcd925ece569a1
luffy01984-png <luffy01984@gmail.com>
2025-12-04 12:11:32 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // CHANGE LA VERSION ICI à chaque déploiement
-const CACHE_NAME = 'renault-trucks-cse-v3.3.2'; 
+const CACHE_NAME = 'renault-trucks-cse-v3.3.3'; 
 const PRECACHE = [
   './',
   './index.html',
